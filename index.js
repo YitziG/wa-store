24a736ef434a93b52eadcc76ca9843c2c9b383c8
YitziG <yitzig@wix.com>
2023-05-03 19:42:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import pkg from 'whatsapp-web.js';
 import dotenv from 'dotenv';
 import {createClient, OAuthStrategy} from '@wix/api-client';
 import {products} from "@wix/stores";
-import {cart} from "@wix/ecom";
+import {cart, currentCart} from "@wix/ecom";
 import axios from "axios";
 
 dotenv.config();
@@ -21,7 +21,7 @@ const wixClient = createClient({
         clientId: process.env.CLIENT_ID
     }),
     modules: {
-        products, cart
+        products, cart, currentCart
     }
 });
 
@@ -40,19 +40,32 @@ const {items: productList} = await wixClient.products.queryProducts().find();
 // Storing user tokens
 const userTokensMap = new Map();
 
+// A map of user ids to their states
 const userStates = new Map();
 
 client.on('message', async (msg) => {
     const userPhone = msg.from;
     await getUserTokens(userPhone);
-    switch (msg.body.trim()) {
-        case msg.body.trim().match(/^([1-9][0-9]*)$/)?.[0]: // Check if the message contains a valid number
+
+    const currentState = userStates.get(userPhone) || {stage: 'start'};
+
+    switch (currentState.stage) {
+        case 'start':
+            const formattedProductsList = productList.map(
+                (product, index) => `${index + 1}. ${product.name}`
+            );
+
+            await msg.reply(`Available products:\n\n${formattedProductsList.join("\n")}\n\nType the number of the product you want to view or add to your cart.`);
+            userStates.set(userPhone, {stage: 'selectProduct'});
+            break;
+        case 'selectProduct':
             const selectedProductIndex = parseInt(msg.body.trim()) - 1;
 
             // Check if the selected product index is within the range of the productList
             if (0 <= selectedProductIndex && selectedProductIndex < productList.length) {
                 const selectedProduct = productList[selectedProductIndex];
-                // Perform actions with the selected product (e.g., display details, add to cart, etc.)
+                currentState.selectedProduct = selectedProduct;
+                userStates.set(userPhone, currentState);
 
                 const imageUrl = selectedProduct.media.mainMedia?.image?.url;
                 if (imageUrl) {
@@ -65,26 +78,69 @@ client.on('message', async (msg) => {
                         console.error('Error fetching the image:', error.message);
                     }
                 }
+
+                await msg.reply(`You've selected ${selectedProduct.name}. What would you like to do next?\n\n1. Add to cart\n2. Purchase\n3. Go back to product list`);
+                userStates.set(userPhone, {...currentState, stage: 'productActions'});
             } else {
                 await msg.reply('Invalid product number. Please select a number from the product list.');
             }
 
             break;
+        case 'productActions':
+            const selectedProduct = currentState.selectedProduct;
+
+            if (msg.body.trim() === '1') {
+                try {
+                    await wixClient.currentCart.addToCurrentCart({
+                        lineItems: [
+                            {
+                                quantity: 1,
+                                catalogReference: {
+                                    catalogItemId: selectedProduct._id,
+                                    appId: "1380b703-ce81-ff05-f115-39571d94dfcd"
+                                }
+                            }]
+                    });
+                } catch (error) {
+                    console.error('Error adding product to cart:', error.message, error.details);
+                    await msg.reply('Error adding product to cart. Please try again later.');
+                    userStates.set(userPhone, {stage: 'start'});
+                    client.emit('message', msg); // Trigger the start stage again by emitting the same message
+                    return;
+                }
+
+                await msg.reply(`${selectedProduct.name} has been added to your cart.`);
+                const userCart = await wixClient.currentCart.getCurrentCart();
+                const cartItems = userCart.lineItems.map((item, index) => `${index + 1}. ${item.productName.translated} (Quantity: ${item.quantity})`);
+                await msg.reply(`Your cart items:\n\n${cartItems.join("\n")}`);
+            } else if (msg.body.trim() === '2') {
+                await msg.reply('You chose to purchase the product. Please proceed with the payment on our website (add your website URL here).');
+                userStates.set(userPhone, {stage: 'start'});
+            } else if (msg.body.trim() === '3') {
+                userStates.set(userPhone, {stage: 'start'});
+                client.emit('message', msg); // Trigger the start stage again by emitting the same message
+            } else {
+                await msg.reply('Invalid option. Please choose a valid action:\n\n1. Add to cart\n2. Purchase\n3. Go back to product list');
+            }
+            break;
+        case 'viewCart':
+            if (msg.body.trim().toLowerCase() === 'back') {
+                userStates.set(userPhone, {stage: 'start'});
+                client.emit('message', msg); // Trigger the start stage again by emitting the same message
+            } else {
+                await msg.reply('Type "back" to return to the product list or proceed with the payment on our website (add your website URL here).');
+            }
+
+            break;
         default:
-
-
-            const formattedProductsList = productList.map(
-                (product, index) => `${index + 1}. ${product.name}`
-            );
-
-            await msg.reply(`Available products:\n\n${formattedProductsList.join("\n")}\n\nType the number of the product you want to view or add to your cart.`);
+            userStates.set(userPhone, {stage: 'start'});
+            client.emit('message', msg); // Trigger the start stage again by emitting the same message
             break;
     }
 });
 
 const getUserTokens = async (userPhone) => {
     let userTokens = userTokensMap.get(userPhone);
-
     if (!userTokens) {
         // Generate new access and refresh tokens for the new visitor
         userTokens = await wixClient.auth.generateVisitorTokens();
