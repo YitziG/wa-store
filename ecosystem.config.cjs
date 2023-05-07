aa590b4023be27473ef00ca2a1868570810071a0
YitziG <yitzig@wix.com>
2023-05-07 21:58:58 +0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'wa-store',
-            script: 'index.mjs',
+            script: './index.mjs',
             instances: 1,
             autorestart: true,
             watch: false,
