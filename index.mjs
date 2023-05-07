bf6c00208e3e74c13278123c4b207526f538fe76
YitziG <yitzig@wix.com>
2023-05-07 21:49:47 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,4 +1,8 @@
-global.fetch = require('node-fetch');
+import fetch from 'node-fetch';
+
+// Assign fetch to the global object
+globalThis.fetch = fetch;
+
 import {initializeWAClient} from "./whatsapp/wa_client.mjs";
 
 (async () => {
